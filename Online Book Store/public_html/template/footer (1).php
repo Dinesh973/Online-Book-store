a089e4a0f141ee7fa32c9f25f40523e9070fdaa0
Dinesh973 <74952487+Dinesh973@users.noreply.github.com>
2021-04-19 20:49:44 +0530

    Add files via upload

---
+++ Online Book Store/public_html/template/footer (1).php
@@ -0,0 +1,59 @@
+      	<hr>
+
+      	<footer>
+        	<div class="text-muted pull-right">
+           <h3>Follow us on</h3><br>
+                <a href="https://www.instagram.com/"> 
+          		<button type="button" class="btn btn-secondary">Instagram</button>
+          		
+          		<a href="https://www.facebook.com/"> 
+          		<button type="button" class="btn btn-primdary">Facebook</button>
+
+          		<a href="https://www.linkedin.com/in/dinesh-s-2a0450200/"> 
+          		<button type="button" class="btn btn-prdimary">LinkedIn</button>
+
+          		<a href="https://www.youtube.com/"> 
+          		<button type="button" class="btn btn-pridmary">Youtube</button><br>
+
+          		<a href="https://www.pinterest.com/"> 
+          		<button type="button" class="btn btn-primdary">Pinterest</button>
+
+          		<a href="https://www.Twitter.com/"> 
+          		<button type="button" class="btn btn-primdary">Twitter</button>
+          		
+        	</div>
+        	<div class="text-muted pull-left">
+        	   
+        	           	   Ajay CSE Bookstores Private Limited<br>
+ Ajay House, 15 Park Street, Annanagar, Chennai 600086.<br><br>
+ <span class="glyphicon glyphicon-phone"></span>&nbsp; +91 9876-543-210<br>
+<br>
+<br><br>
+
+<a href="https://dinesh30100.000webhostapp.com"><h5>HOME</h5></a><br><br><br><br>
+
+<a href="#" class="fa fa-facebook"></a> 
+<div class="stickyicon-list">  
+<a href="facebook.com" class="facebook">   
+<i class="fa fa-facebook"></i>   
+</a>
+</div>
+<hr>
+<div class = "footer-content"></div>
+        	    <div class = "footer-bottom">
+        	        &copy; dinesh30100.000webhostapp.com | Made by Dinesh Inc.
+        	    </div>
+
+          		
+        	</div>
+      	</footer>
+    </div> <!-- /container --> 
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="./bootstrap/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
